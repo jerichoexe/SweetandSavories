82911c82be04b7ebc2d3444115470bbe6b9cfa9d
jerichoexe <117624850+jerichoexe@users.noreply.github.com>
2022-12-21 09:26:49 -0600

    :+1:
    All changes made as of 9:26am, Dec 21

---
+++ Sweets&Savories/index.js
@@ -0,0 +1,10 @@
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-bar').onclick=() => {
+    navbar.classList.toggle('active');
+}
+
+
+let search = document.querySelector('.search');
+document.querySelector('#search').onclick=() => {
+    search.classList.toggle('active');
+}
